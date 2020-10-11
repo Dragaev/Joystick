6578746108291841c329bc30e96d9c393d369a6d
Max Dragaev <dragaev@MacBook-Air-Max.local>
2020-10-11 13:48:23 +0700

    Initial commit

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float speedMove=5;
+    public float jumpForce = 10;
+    private Vector3 moveVector;
+
+    private Joystick joystick;
+    protected JoyButton joyButton;
+    protected bool jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = GameObject.FindGameObjectWithTag("JoystickTag").GetComponent<Joystick>();
+        joyButton = FindObjectOfType<JoyButton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        moveVector.x = joystick.Horizontal() * speedMove;
+        moveVector.y = joystick.Vertical() * speedMove;
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(moveVector.x + Input.GetAxis("Horizontal") * 100f, rigidbody.velocity.y, moveVector.y + Input.GetAxis("Vertical") * 100f);
+
+        if (!jump && joyButton.Pressed)
+        {
+            jump = true;
+            rigidbody.velocity += Vector3.up * jumpForce;
+        }
+        if (jump && !joyButton.Pressed)
+        {
+            jump = false;
+        }
+    }
+    
+}
